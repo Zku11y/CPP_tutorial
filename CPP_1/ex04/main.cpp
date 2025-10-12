90c87746d1508ad73ec44a8e8842b6fbc81f1c72
Zku11y <craneflam159@gmail.com>
2025-10-12 23:41:39 +0100

    ex04 done

--- CPP_1/ex04/main.cpp
+++ CPP_1/ex04/main.cpp
@@ -4,12 +4,29 @@ int main(int ac, char **av){
     if(ac != 4)
         return(std::cout << "need 4 arguments!" << std::endl, 1);
     std::ifstream file;
+    std::ofstream outfile;
     file.open(av[1]);
     if(file.fail())
-        return(std::cerr << "error while opening file\n", 1);
+        return(std::cerr << "error while opening infile\n", 1);
+    outfile.open((std::string(av[1]) + ".replace").c_str());
+    if(outfile.fail())
+        return(std::cerr << "error while opening outfile\n", 1);    
     std::string input;
+    std::string to_find = av[2];
+    std::string to_change = av[3];
+    size_t pos;
     while(std::getline(file, input))
     {
-        while
+        pos = input.find(to_find);
+        while(pos != std::string::npos)
+        {
+            outfile << input.substr(0, pos) << to_change;
+            input.erase(0, pos + to_find.length());
+            pos = input.find(to_find);
+        }
+        outfile << input;
+        if(!file.eof())
+            outfile << '\n';
     }
+    return 0;
 }
